dab257c56389c734c65a1898ac3a0b486dbcb741
Winnie Hsu <winnie860403@gmail.com>
2020-05-22 02:13:55 +0800

    Updates

---
+++ precache-manifest.10966cc6a0b9902ca8025a81193c63c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0227cc0d3dac4d63f480b7b4ad94409",
+    "url": "/linggle-web/index.html"
+  },
+  {
+    "revision": "3d3af3fe8a2505ee466e",
+    "url": "/linggle-web/static/css/main.aef67360.chunk.css"
+  },
+  {
+    "revision": "e8dee33ac3785e2e84af",
+    "url": "/linggle-web/static/js/2.501f7869.chunk.js"
+  },
+  {
+    "revision": "296362f3435e3c4e1832bddadf6a9199",
+    "url": "/linggle-web/static/js/2.501f7869.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d3af3fe8a2505ee466e",
+    "url": "/linggle-web/static/js/main.24cbb6b4.chunk.js"
+  },
+  {
+    "revision": "c6f898edd9403d92eb75",
+    "url": "/linggle-web/static/js/runtime-main.b62cfb97.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-implement/precache-manifest.cadfdb02692213e70456afeba44c60c0.js"
+  "/linggle-web/precache-manifest.10966cc6a0b9902ca8025a81193c63c7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-implement/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/linggle-web/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
